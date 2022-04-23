06f0edd1d2fa3567ffc5b3efabb92bf6da986231
Mercynenoh <mercychepngenoh74@gmail.com>
2022-04-23 23:17:36 +0300

    create account function

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ def create_user(username,password):
     Function to create a new user
     '''
     newUser = User(username,password)
-    return myUser
+    return newUser
 
 def save_user(user):
     '''
@@ -70,5 +70,64 @@ def generate_a_password(passwordLength):
     Function that generates random password of 8 characters
     '''
     return Credentials.generate_password(passwordLength)
+
+def main():
+    print('*'*10 + "Hey, Welcome" + '*'*10)
+    print('--'*22)
+    print('Have you been having a problem remembering your passwords?\n Worry no more, You can manage your passwords here')
+    print('--'*22)
+    current_user= input ('What is your name?\n')
+    print(f'\n Hello {current_user},')
+    while True:
+                print("Use these short codes for navigation: \n CA - Create Account \n LO - Log In \n DA - Delete your account \n EX - Exit ")
+
+                my_input = input().upper()
+
+                if my_input == 'CA':
+                    print('\n CREATE YOUR ACCOUNT')
+                    print('-'*10)
+
+                    print('Please enter your desired Username:\n')
+                    username = input()
+
+                    # print(" "*4 + "*the username contains alphabetical letters only and no spaces*")
+                    
+
+                    print(' Please enter your password\n')
+                    password = input()
+
+                    save_user(create_user(username, password))
+                   
+                    print(f"New User {username}  has been created")
+                    
+
+                if my_input =="LO":
+                    print("Already have an account? Sign in\n")
+                    print("-"*10)
+
+                    print("Enter your username")
+                    username = input().strip(' ').capitalize()
+                    print("Enter your password")
+                    password = input().strip(' ')
+
+                    print("What would you like to do?")
+                else:
+                
+                    print("\nUse these short codes to create new credentials: \n CC: create new credentials \n FC: find a credential \n DC: delete a credential \n SC: see all credentials \n LO: log out")
+                     
+                     my_credentials = input().upper()
+
+                if my_credentials = "CC"
+                print('')
+                if 
+
+
+
+
+                    
+
+
+if __name__ == '__main__':
+    main()
 
 
